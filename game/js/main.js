f3bb51ce9104b293c730a534128d9fd853102cb6
Charles Newey <charlie@assemblyco.de>
2014-04-10 04:40:58 +0100

    Update main

--- game/js/main.js
+++ game/js/main.js
@@ -1,12 +1,20 @@
+/*global Character, Player, Sprite*/
 /*jslint browser: true*/
 
-//Javascript: The Good Parts pg 22 (Chapter 3: Objects)
-if (typeof Object.create !== 'function') {
-    Object.create = function (o) {
-        'use strict';
-        
-        var F = function () {};
-        F.prototype = o;
-        return new F();
-    };
-}
+window.onload = function () {
+    'use strict';
+    
+    // Set up horrible globals
+    var game, canvas, ctx;
+    
+    // Set up main Game variable
+    game = {};
+
+    // Load canvas data into a variable
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    // Set up Player
+    game.player = new Player(100, new Sprite('face_sprite'));
+    game.player.sprite.draw(ctx);
+};
